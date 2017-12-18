14bfdd3b5f535b69dfc1cccfaf81075dae38fc83
Ivan Vučica <ivan@vucica.net>
2017-12-18 18:57:59 +0000

    example: Check initializations for errors. Call SDL_Quit() before quitting.

--- example/full_repo/full_repo.c
+++ example/full_repo/full_repo.c
@@ -1,6 +1,13 @@
 #include <SDL/SDL.h>
+#include <stdio.h>
 
 int main() {
-  SDL_Init(SDL_INIT_VIDEO);
+  if((SDL_Init(SDL_INIT_VIDEO)==-1))
+    {
+      fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
+      exit(-1);
+    }
+
+  SDL_Quit();
   return 0;
 }
--- example/simple/simple.c
+++ example/simple/simple.c
@@ -1,6 +1,13 @@
 #include <SDL/SDL.h>
+#include <stdio.h>
 
-int main() {
-  SDL_Init(SDL_INIT_VIDEO);
+int main(int argc, char ** argv) {
+  if((SDL_Init(SDL_INIT_VIDEO)==-1))
+    {
+      fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
+      exit(-1);
+    }
+
+  SDL_Quit();
   return 0;
 }
