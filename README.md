831e4445c21b3d1e8fed11fba16cccdfea3349f3
Ivan Vučica <ivan@vucica.net>
2024-02-02 12:56:03 +0000

    Basic fetch script for Debian dependencies.
    Actual binaries and code have not been checked in in this first commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+Archive of Debian's .deb binary packages (and sources to comply with any
+possible license requirements) required for Bazel rules for libsdl1.2 to
+work.
+
+This is done in a separate branch to avoid ballooning the size of the
+code branch.
+
+Initial commit does not contain any actual libraries.
---
+++ fetch.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PACKAGES_X11="libx11-dev libxext-dev libxrandr-dev libxrender-dev"
+PACKAGES_EXTRA="libgl-dev libglu1-mesa-dev libasound2-dev libalsaplayer-dev"
+
+PACKAGES="${PACKAGES_X11} ${PACKAGES_EXTRA}"
+
+for pkg in ${PACKAGES} ; do
+(
+  tput bold
+  tput rev
+  tput setaf 1  # red
+  echo Updating $pkg
+  tput setaf 9  # default color
+  tput sgr0  # default attributes
+  rm -rf $pkg/
+  mkdir -p $pkg/
+  cd $pkg/
+  apt-get download $pkg
+  apt-get source $pkg
+)
+done
+
+tput bold
+tput rev
+tput setaf 2
+echo "Before committing, run 'git add .' or similar to stage all changes."
+echo
+echo "Update '.gitignore' if necessary."
+tput setaf 9
+tput sgr0
