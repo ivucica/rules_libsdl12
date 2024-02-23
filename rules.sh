d33c39ae68a5d6a4ef04c5f748ca18bfec2d7748
Ivan Vučica <ivan@vucica.net>
2024-02-23 18:20:39 +0000

    Rule generator for the current iteration of the x11 helper.

---
+++ rules.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+find . -type f -name '*.deb' | xargs -i bash -c "echo '    x11_deb_repository('; echo '        name=\"'\$(dirname {} | sed s@^./@@)'\",'; echo '        urls=[\"https://github.com/ivucica/rules_libsdl12/raw/\" +  master_deb_hash + \"/'\$(echo {} | sed s@^./@@)'\"],'; echo '        sha256=\"'\$(sha256sum {} | cut -d' ' -f1)'\",'; echo '    )'; echo ''"
+
