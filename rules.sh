a1566651c0939d01a3f8f18a17ecc7df114be8c0
Ivan Vučica <ivan@vucica.net>
2024-04-30 15:25:46 +0000

    rules.sh: urlencode the .deb filename using jq.
    See https://stackoverflow.com/a/34407620.

--- rules.sh
+++ rules.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find . -type f -not -path '*/.git/*' -name '*.deb' | xargs -i bash -c "echo '    x11_deb_repository('; echo '        name = \"'\$(dirname {} | sed s@^./@@)'\",'; echo '        urls = [\"https://github.com/ivucica/rules_libsdl12/raw/\" +  master_deb_hash + \"/'\$(echo {} | sed s@^./@@)'\"],'; echo '        sha256 = \"'\$(sha256sum {} | cut -d' ' -f1)'\",'; echo '    )'; echo ''"
+find . -type f -not -path '*/.git/*' -name '*.deb' | xargs -i bash -c "echo '    x11_deb_repository('; echo '        name = \"'\$(dirname {} | sed s@^./@@)'\",'; echo '        urls = [\"https://github.com/ivucica/rules_libsdl12/raw/\" +  master_deb_hash + \"/'\$(echo -n {} | sed s@^./@@ | jq -sRr @uri)'\"],'; echo '        sha256 = \"'\$(sha256sum {} | cut -d' ' -f1)'\",'; echo '    )'; echo ''"
 
